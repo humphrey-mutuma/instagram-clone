f42b1ffca9f308823e279b7c5a9ec9c93ec52751
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-14 21:48:22 +0300

    firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,17 @@
-
+import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyASDnyjZbFmaCWxu4EEDugms323ji9771Q",
   authDomain: "instagram-clone-595a1.firebaseapp.com",
   projectId: "instagram-clone-595a1",
   storageBucket: "instagram-clone-595a1.appspot.com",
   messagingSenderId: "948231976143",
   appId: "1:948231976143:web:a293d77607d5fef0ca73ba",
-  measurementId: "G-J297M4C6ER"
-};
+  measurementId: "G-J297M4C6ER",
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
