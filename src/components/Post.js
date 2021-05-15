66a18610c2e18394db903324e0345539b0bedfa5
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-05-15 20:30:05 +0300

    Add comments complete.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,40 @@
 import { Avatar } from "@material-ui/core";
-import React from "react";
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
 import "./Post.css";
+import firebase from "firebase";
 
-const Post = ({ username, imageUrl, caption }) => {
-  
+const Post = ({ postId,user, username, imageUrl, caption }) => {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState(" ");
+
+    useEffect(() => {
+      let unsubscribe;  
+      if (postId) {
+        unsubscribe = db
+          .collection("posts")
+          .doc(postId)
+          .collection("comments")
+          .orderBy("timestamp", "desc")
+          .onSnapshot((snapshot) => {
+            setComments(snapshot.docs.map((doc) => doc.data()));
+          });
+      }
+
+      return () => {
+        unsubscribe();
+      };
+    }, [postId]);
+    
+ const postComment = (event) => {
+   event.preventDefault();
+   db.collection("posts").doc(postId).collection("comments").add({
+     text: comment,
+     username: user.displayName,
+     timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+   });
+   SetComment("");
+ };
   return (
     <div className="post">
       <div className="post__header">
@@ -20,6 +51,31 @@ const Post = ({ username, imageUrl, caption }) => {
         <strong>{username}</strong>: {caption}
       </h4>
 
+      <div className="post__comments">
+        {comments.map((comment) => (
+          <p>
+            <strong>{comment.username}</strong> {comment.text}
+          </p>
+        ))}
+      </div>
+
+      <form className="post__commentBox">
+        <input
+          className="post__input"
+          type="text"
+          placeholder="Add a comment"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+        <button
+          className="post__button"
+          disabled={!comment}
+          type="submit"
+          onClick={postComment}
+        >
+          Post
+        </button>
+      </form>
     </div>
   );
 };
